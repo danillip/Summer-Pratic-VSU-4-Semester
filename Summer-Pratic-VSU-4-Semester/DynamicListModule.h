093936d689b60c2eab14f99a4be8d93cc75efa95
GreateMaple <162444795+danillip@users.noreply.github.com>
2024-07-10 00:05:43 +0300

    Delete Summer-Pratic-VSU-4-Semester directory

--- Summer-Pratic-VSU-4-Semester/DynamicListModule.h
+++
@@ -1,9 +0,0 @@
-// DynamicListModule.h
-#ifndef DYNAMICLISTMODULE_H
-#define DYNAMICLISTMODULE_H
-
-#include <Windows.h>
-
-void GenerateDynamicList(HWND hWndParent);
-
-#endif // DYNAMICLISTMODULE_H
